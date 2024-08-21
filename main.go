2c96c0f669cff03ba4e6a575e10c017dc34f0161
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-21 19:40:09 -0300

    create getTodos function

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/gofiber/fiber"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -46,4 +48,46 @@ func main() {
 	fmt.Println("Connected to MongoDB")
 
 	collection = client.Database("react-go-tutorial").Collection("todos")
+
+	app := fiber.New()
+
+	app.Get("/api/todos", getTodos) 
+	app.Post("/api/todos", createTodo)
+	app.Put("/api/todos/:id", updateTodo)
+	app.Delete("/api/todos/:id", deleteTodo)
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3001"
+	}
+
+	log.Fatal(app.Listen("0.0.0.0" + port))
+
 }
+
+// Create todo
+func getTodos(c *fiber.Ctx) error {
+	var todos []Todo
+	cursor, err := collection.Find(context.Background(), bson.M{})
+
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	for cursor.Next(context.Background()) {
+		var todo Todo
+		if err := cursor.Decode(&todo); err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		}
+
+		todos = append(todos, todo)
+	}
+
+	return c.JSON(todos)
+}
+
+
+// func createTodo(c *fiber.Ctx) error {}
+// func updateTodo(c *fiber.Ctx) error {}
+// func deleteTodo(c *fiber.Ctx) error {}
