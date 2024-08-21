2852814cfbbe128ae74f858e5b037a64b42bf9b3
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-21 20:32:51 -0300

    install tanstack query and initialize it in the main.jsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,16 @@ import App from './App.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
