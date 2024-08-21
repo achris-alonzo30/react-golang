812964147bd634bc882711f0ad27d843ed5302d7
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-21 18:56:04 -0300

    create POST request for creating new todo

--- main.go
+++ main.go
@@ -17,8 +17,27 @@ func main() {
 	fmt.Println("Hello, World!")
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"message": "Hello, World!"})
+	})
+
+	// Create a new todo
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+		if err := c.BodyParser(todo); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid request"})
+		}
+		if todo.Title == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo title is required"})
+		}
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		
+
+		return c.Status(201).JSON(todo)
 	})
 
 	log.Fatal(app.Listen(":3000"))
