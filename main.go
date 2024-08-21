9abce00d25091d4f91134de07b60e44425d303b9
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-21 18:40:49 -0300

    create Todo type

--- main.go
+++ main.go
@@ -7,9 +7,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"message": "Hello, World!"})
+	})
 
 	log.Fatal(app.Listen(":3000"))
 }
