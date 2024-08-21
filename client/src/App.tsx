afa4566300a2b9333704bc315328f28ce42c5960
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-21 20:13:37 -0300

    configure App.tsx to structure the components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
+import { Container, Stack } from "@chakra-ui/react";
+import { Navbar } from "./components/Navbar";
+import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
+
 export default function App() {
   return (
-    <></>
+    <Stack h="100vh" w="100vw">
+      <Navbar />
+      <Container>
+        <TodoForm />
+        <TodoList />
+      </Container>
+    </Stack> 
   )
 }
